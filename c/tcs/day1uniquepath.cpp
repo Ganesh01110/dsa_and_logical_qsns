5d307e81ef71b97655072fabe2fad3792cff6b1e
Ganesh sahu <sahug8279@gmail.com>
2024-07-13 23:23:48 +0530

    unique path  question for problem

---
+++ c/tcs/day1uniquepath.cpp
@@ -0,0 +1,4 @@
+// unique path
+// there is  a robot on an m*n grid. the robot is initially located at the top-left corner i.e grid[0][0].
+// the robot tries to move to bottom right corner i.e grid[m-1][n-1] the robot can either move either right or down at a time . 
+// given two intiger m and n . return the path of possible unique path that the robot can take to reah bottom right corner. the test cases are generated so that the answer will be less that or equal to 2*10^9
