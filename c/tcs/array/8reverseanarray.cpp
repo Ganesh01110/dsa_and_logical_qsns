ca2db9ba5234ac0efe52c52a39f0b4142e79c0ee
Ganesh sahu <sahug8279@gmail.com>
2024-07-14 23:29:00 +0530

    add arrayreverse approach-3 n 4

--- c/tcs/array/8reverseanarray.cpp
+++ c/tcs/array/8reverseanarray.cpp
@@ -69,3 +69,80 @@ int main() {
    reverseArray(arr, n);
    return 0;
 }
+
+
+// // approach-3
+// Solution 3: Recursive method
+
+// Approach: The recursive method has an approach almost similar to the iterative one. The approach has been broken down into some steps for simplicity.
+
+// Create a function that takes an array, start index, and end index of the array as parameters.
+// Swap the elements present  at the start and end index, 
+// The portion of the array left to be reversed is arr[start+1,end-1]. Make a recursive call to reverse the rest of the array. While calling recursion pass start +1  and ends - 1 as parameters for the shrunk array. Repeat step 2.
+// Continue recursion as long as the ‘start < end’ condition is satisfied. This is the base case for our recursion.
+
+// Time Complexity: O(n)
+
+// Space Complexity: O(1)
+//Function to print array
+void printArray(int arr[], int n) {
+   cout << "The reversed array is:- " << endl;
+   for (int i = 0; i < n; i++) {
+      cout << arr[i] << " ";
+   }
+}
+//Function to reverse array using recursion
+void reverseArray(int arr[], int start, int end) {
+   if (start < end) {
+      swap(arr[start], arr[end]);
+      reverseArray(arr, start + 1, end - 1);
+   }
+}
+int main() {
+   int n = 5;
+   int arr[] = { 5, 4, 3, 2, 1 };
+   reverseArray(arr, 0, n - 1);
+   printArray(arr, n);
+   return 0;
+}
+
+// // approach-4
+// Solution 4: Using library function (New Approach)
+
+// Approach: C++ and Java have inbuilt functions to reverse an array.
+
+// For C++:-
+
+// The std::reverse function in C++ is predefined in a header file algorithm.
+
+// Syntax: array_name.reverse(BidirectionalIterator first, BidirectionalIterator last)
+
+// Bidirectional iterators first and last indicate the starting and ending positions in the array. Elements that lie in this range are reversed. Note that this range includes the first but excludes the last element.
+
+// Time Complexity: O(n)
+
+// Space Complexity: O(1)
+
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+//Function to print array
+void printArray(int arr[], int n) {
+   cout << "The reversed array is:- " << endl;
+   for (int i = 0; i < n; i++) {
+      cout << arr[i] << " ";
+   }
+}
+//Reverse array using library function
+void reverseArray(int arr[], int n) {
+   //Reversing elements from index 0 to n-1 
+   reverse(arr, arr + n);
+}
+int main() {
+   int n = 5;
+   int arr[] = { 5, 4, 3, 2, 1 };
+   reverseArray(arr, n);
+   printArray(arr, n);
+   return 0;
+}
