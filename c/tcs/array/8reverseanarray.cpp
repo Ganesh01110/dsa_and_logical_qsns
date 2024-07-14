92ebff2a487833d213b7a8ee6800b1df4f165657
Ganesh sahu <sahug8279@gmail.com>
2024-07-14 23:13:58 +0530

    add arrayreverse approach-2

--- c/tcs/array/8reverseanarray.cpp
+++ c/tcs/array/8reverseanarray.cpp
@@ -32,3 +32,40 @@ int main() {
    reverseArray(arr, n);
    return 0;
 }
+
+// approach 2
+// Space-optimized iterative method
+
+// Approach: Unlike the previous method we use the same array to obtain the result. Follow the steps below.
+
+// Keep a pointer p1  at the first index and another p2 at the last index of the array. 
+// Swap the elements pointed by p1 and p2, Post swapping increment p1 and decrement p2.
+// This process is repeated for only the first n/2 elements where n is the length of array.
+// Note: Swapping all the n elements instead of n/2 elements leaves the array unaltered.
+
+// Time Complexity: O(n), single-pass involved.
+
+// Space Complexity: O(1) 
+
+//Function to print array
+void printArray(int arr[], int n) {
+   cout << "The reversed array is:- " << endl;
+   for (int i = 0; i < n; i++) {
+      cout << arr[i] << " ";
+   }
+}
+//Function to reverse array 
+void reverseArray(int arr[], int n) {
+   int p1 = 0, p2 = n - 1;
+   while (p1 < p2) {
+      swap(arr[p1], arr[p2]);
+      p1++; p2--;
+   }
+   printArray(arr, n);
+}
+int main() {
+   int n = 5;
+   int arr[] = { 5, 4, 3, 2, 1};
+   reverseArray(arr, n);
+   return 0;
+}
