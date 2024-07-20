e66d3d9590dd47426773fb9e0a8e3ffb5069a24c
Ganesh sahu <sahug8279@gmail.com>
2024-07-20 10:38:08 +0530

    add octalltobinary solution

--- c/number_system/octaltobinary.cpp
+++ c/number_system/octaltobinary.cpp
@@ -16,3 +16,49 @@
 // For Decimal to Binary Conversion
 
 // For Decimal to Binary Conversion, we will divide the given number by 2 ( Since the Binary Numbers System has 2 digits in use ) repeatedly, and its remains will be stored till the number becomes zero.
+
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int OctaltoDecimal(int Octal)
+{
+    int Decimal = 0;
+    int i = 0;
+    while (Octal != 0)
+    {
+        int rem = Octal % 10;
+        Decimal += rem * pow(8, i);
+        i++;
+        Octal /= 10;
+    }
+    return Decimal;
+}
+int DecimaltoBinary(int decimal)
+{
+    int Binary = 0;
+    int i = 0;
+    while (decimal != 0)
+    {
+        int rem = decimal % 2;
+        Binary += (rem * pow(10, i));
+        i++;
+        decimal = decimal / 2;
+    }
+    return Binary;
+}
+int main()
+{
+    int Octal = 345;
+    int Decimal = OctaltoDecimal(Octal);
+    cout << "The binary conversion of the given octal number is "<<DecimaltoBinary(Decimal) << endl;
+    return 0;
+}
+
+// Output:
+
+// The binary conversion of the given octal number is 11100101
+
+// Time Complexity: O(log n) where n is the number 
+
+// Space Complexity: O(1)
