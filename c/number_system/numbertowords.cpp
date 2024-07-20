263d22d4b6cea53691f1a7b1673c22e3b90a9763
Ganesh sahu <sahug8279@gmail.com>
2024-07-20 11:58:22 +0530

    add numbertowords solutions

--- c/number_system/numbertowords.cpp
+++ c/number_system/numbertowords.cpp
@@ -24,3 +24,66 @@
 // Case 4: When 4 digits are present
 
 // Just add thousand to thousand place and further process will remain same as 3 digit number.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void convertnumintoword(string str)
+{
+  string singledigit[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+  string twodigits[] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "ninteen", "twelve"};
+  string tensmultiple[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninty"};
+  string tenspower[] = {"hundred", "thousand"};
+  if (str.length() == 0)
+  {
+    cout << "" << endl;
+    return;
+  }
+  else if (str.length() == 1)
+  {
+    cout << singledigit[str[0] - '0'] << endl;
+    return;
+  }
+  int len = str.length();
+  for (int i = 0; i < str.length(); i++)
+  {
+    if (len > 2)
+    {
+      if (str[i] - '0' != 0)
+      {
+        cout << singledigit[str[i] - '0'] << " ";
+        cout << tenspower[len - 3] << " ";
+      }
+      len--;
+    }
+    else
+    {
+      if (str[i] - '0' == 1)
+      {
+        cout << twodigits[str[i + 1] - '0'] << " ";
+        return;
+      }
+      else if (str[i] - '0' != 0)
+      {
+        cout << tensmultiple[str[i] - '0'] << " ";
+        if (str[i + 1] - '0' != 0)
+          cout << singledigit[str[i + 1] - '0'] << " ";
+        return;
+      }
+    }
+  }
+}
+
+int main()
+{
+  string str = "9090";
+  convertnumintoword(str);
+  return 0;
+}
+
+
+// Output: nine thousand ninty
+
+// Time Complexity: O(N) which is the length of string
+
+// Space Complexity: O(1)
