1ea4a2ea870ccbbda799bd6b46b4145f1e320077
Ganesh sahu <sahug8279@gmail.com>
2024-07-26 00:04:26 +0530

    add cbinher-1n2

---
+++ c/oops/cbinher1.cpp
@@ -0,0 +1,46 @@
+// yt-codebeauty
+#include <iostream>
+using std::string;
+
+class Employee{
+    public :
+    string Name;
+    string Company;
+    int Age;
+
+    void IntroduceYOurself(){
+        std::cout<<"Namme -"<< Name << std::endl;
+
+        std::cout<<"company -"<< Company<< std::endl;
+
+        std::cout<<"age -"<< Age << std::endl;
+    }
+
+    // rules for constructor
+    // 1->no return type
+    // 2->same nme as class
+    // 3-> need to b epublic but it is not always true
+    // goal of this constructor to create object for this class
+    Employee(string name, string company, int age) {
+        Name = name; 
+        Company = company;
+        Age = age;
+
+    }
+};
+
+int main(){
+
+    // Employee employee1; //here also a constructor revoked without even defining , it can be shown if comment out next 3 lines.->called default constructor
+    // // when a constructor made by us the default constructor stops by default
+    // employee1.Name= "ganesh";
+    // employee1.Company="yt";
+    // employee1.Age=23;
+
+    Employee employee1 = Employee("ganesh"," yt" , 24);
+    employee1.IntroduceYOurself();
+
+    Employee employee2 = Employee("abc","y1-2" , 21);
+    employee2.IntroduceYOurself();
+//  there is a better way to contrucct object , as it will be a problem if we want to create multiple object
+}
---
+++ c/oops/cbinher2.cpp
@@ -0,0 +1,60 @@
+// yt-codebeauty
+#include <iostream>
+using std::string;
+
+class Employee{
+private:
+    string Name;
+    string Company;
+    int Age;
+
+    // private is the default, if nothing defined
+    // need to define setter and getter method for this
+ public :
+    void setName(string name) {
+        Name = name;
+    }
+    string getName(){
+        return Name;
+    }
+
+     void setCompany(string company) {
+        Company = company;
+    }
+    string getCompany(){
+        return Company;
+    }
+
+     void setAge(string age) {
+        Age = age;
+    }
+    int getAge(){
+        return Age;
+    }
+    
+    void IntroduceYOurself(){
+        std::cout<<"Namme -"<< Name << std::endl;
+
+        std::cout<<"company -"<< Company<< std::endl;
+
+        std::cout<<"age -"<< Age << std::endl;
+    }
+
+    Employee(string name, string company, int age) {
+        Name = name; 
+        Company = company;
+        Age = age;
+
+    }
+};
+
+int main(){
+
+
+    Employee employee1 = Employee("ganesh"," yt" , 24);
+    employee1.IntroduceYOurself();
+
+    Employee employee2 = Employee("abc","y1-2" , 21);
+    employee2.IntroduceYOurself();
+
+}
