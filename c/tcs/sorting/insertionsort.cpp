856f731761f7b354505f3ca66f93bd172e025d84
Ganesh sahu <sahug8279@gmail.com>
2024-07-28 12:52:12 +0530

    add qsn

---
+++ c/tcs/sorting/insertionsort.cpp
@@ -0,0 +1,14 @@
+// Given an array of N integers, write a program to implement the Insertion sorting algorithm.
+
+// Example 1:
+// Input: N = 6, array[] = {13,46,24,52,20,9}
+// Output: 9,13,20,24,46,52
+// Explanation: 
+// After sorting the array is: 9,13,20,24,46,52
+
+// Approach: 
+
+// Select an element in each iteration from the unsorted array(using a loop).
+// Place it in its corresponding position in the sorted part and shift the remaining elements accordingly (using an inner loop and swapping).
+// The “inner while loop” basically shifts the elements using swapping.
+
