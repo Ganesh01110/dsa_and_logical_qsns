fe45643a8d97c648cde42d64a6f6a09537232ae0
Ganesh sahu <sahug8279@gmail.com>
2024-07-30 18:29:15 +0530

    pattern

--- LB/4_patterns.cpp
+++ LB/4_patterns.cpp
@@ -1,19 +1,20 @@
-#include <streamio.h>
+#include <iostream>
 
 using namespace std;
 
 // * * *
 // * * *
 // * * * row=col
-void RowColPattern{
-    int num =5;
+void rowColPattern(){
+    int num = 5;
     int i=0;
-    while( i<num){
+    while(i < num){
         int j=0;
         while(j<num){
-            cout<< "* "<< endl;
+            cout<< "* ";
             j=j+1;
         }
+        cout<< endl;
         i=i+1;
     }
 }
@@ -21,21 +22,64 @@ void RowColPattern{
 // * 
 // * * 
 // * * * 
-void upTrianglePattern{
+void upTrianglePattern(){
+     cout<< "____________"<<endl;
     int num =5;
     int i=0;
     while( i<num){
         int j =0;
         while(j<=i){
-            cout<< "* "<< endl;
+            cout<< "* ";
             j=j+1;
         }
+         cout<< endl;
         i=i+1;
     }
 }
 
 
-int main{
-  RowColPattern();
-  upTrianglePattern();
+// * * *
+// * * 
+// *  
+void downTrianglePattern(){
+    cout<< "____________"<<endl;
+    int num =5;
+    int i = num;
+    while( i>0){
+        int j = 0;
+        while(j<i){
+            cout<< "* ";
+            j=j+1;
+        }
+         cout<< endl;
+        i=i-1;
+    }
+}
+
+// 1
+// 1 2  
+// 1 2 3
+void upNumTrianglePattern(){
+    cout<< "____________"<<endl;
+    int num =5;
+    int i = 1;
+    while( i<=num){
+        int j = 1;
+        while(j<=i){
+            cout<< j << " " ;
+            j=j+1;
+        }
+         cout<< endl;
+        i=i+1;
+    }
+}
+
+
+int main() {
+   rowColPattern();
+   upTrianglePattern();
+   downTrianglePattern();
+   upNumTrianglePattern();
+
+  return 0;
 }
