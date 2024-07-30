0b6b8bfc7c3fe12235cb7150626a1ee6f8ec7fed
Ganesh sahu <sahug8279@gmail.com>
2024-07-30 18:54:00 +0530

    pattern

--- LB/4_patterns.cpp
+++ LB/4_patterns.cpp
@@ -73,6 +73,64 @@ void upNumTrianglePattern(){
         i=i+1;
     }
 }
+ 
+// 1 2 3
+// 1 2  
+// 1 
+void downNumTrianglePattern(){
+    cout<< "____________"<<endl;
+    int num =5;
+    int i = num;
+    while( i>0){
+        int j = 1;
+        while(j<=i){
+            cout<< j << " " ;
+            j=j+1;
+        }
+         cout<< endl;
+        i=i-1;
+    }
+}
+
+// 1
+// 2 3  
+// 4 5 6
+void upcountTrianglePattern(){
+    cout<< "____________"<<endl;
+    int num =5;
+    int i = 1;
+    int count =1;
+    while( i<=num){
+        int j = 1;
+        while(j<=i){
+            cout<< count << " " ;
+            count++;
+            j=j+1;
+        }
+         cout<< endl;
+        i=i+1;
+    }
+}
+
+// 6
+// 5 4  
+// 3 2 1
+void downcountTrianglePattern(){
+    cout<< "____________"<<endl;
+    int num =5;
+    int i = num;
+    int count =(num*(num+1))/2;
+    while( i>=0){
+        int j = 1;
+        while(j<=i){
+            cout<< count << " " ;
+            count--;
+            j=j+1;
+        }
+         cout<< endl;
+        i=i-1;
+    }
+}
 
 
 int main() {
@@ -80,6 +138,9 @@ int main() {
    upTrianglePattern();
    downTrianglePattern();
    upNumTrianglePattern();
+   downNumTrianglePattern();
+   upcountTrianglePattern();
+   downcountTrianglePattern();
 
   return 0;
 }
