57d0c7a1fec80d3bdaa0fe96ea4c1cd197dd39b8
Ganesh sahu <sahug8279@gmail.com>
2024-08-03 22:01:41 +0530

    add lcr10

--- LB/10_arrayintersection.cpp
+++ LB/10_arrayintersection.cpp
@@ -23,3 +23,27 @@ vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, i
     }
     return ans;
 }
+
+// or
+// as brute force->compare all element so use if condition to make a break where ever lloop finnds an higher number
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{ 
+    vector<int> ans;
+    for(int i=0;i<n;i++){
+        int element = arr1[i];
+        for(int j=0;j<n;j++){
+
+          if(element<arr2[j]){
+            break;
+          }
+          
+          if(element==arr2[j]){
+            ans.push_back(element);
+            arr2[j]=INT_MIN;
+            break;
+          }
+        }
+    }
+
+    return ans;
+}
