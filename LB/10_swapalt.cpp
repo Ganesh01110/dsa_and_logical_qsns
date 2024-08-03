229b6123a5f9c4cc0efb1b98196385409af3c1e9
Ganesh sahu <sahug8279@gmail.com>
2024-08-03 20:24:48 +0530

    add lcr10

--- LB/10_swapalt.cpp
+++ LB/10_swapalt.cpp
@@ -4,9 +4,9 @@ using namespace std;
 void printingIntarray(int arr[] , int size){
     
     for(int i=0 ;i<size;i++){
-        cout<<arr[i]<<endl;
+        cout<<arr[i]<<" ";
     }
-    cout<<"printing done of int array"<<endl;
+    cout<<"\nprinting done of int array"<<endl;
 }
 
 void swapalt(int arr[],int size){
@@ -23,10 +23,25 @@ void swapalt(int arr[],int size){
    printingIntarray(arr,size);
 }
 
+void swapbytemp(int arr[],int size){
+    int swap=0;
+    for(int i=0;i<size;i+=2){
+        if (i+1<size){
+            swap=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=swap;
+        }
+    }
+
+    cout<<"after custom swap function"<<endl;
+    printingIntarray(arr,size);
+}
+
 int main(){
     int arr[]={1,2,5,8,9,70,43};
     int size= sizeof(arr)/sizeof(arr[0]);
     swapalt(arr,size);
+    swapbytemp(arr,size);
 
     int arr2[]={4,5,7,8,9,99};
     int size2= sizeof(arr2)/sizeof(arr2[0]);
